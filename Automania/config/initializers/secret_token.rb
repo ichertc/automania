1860a6031db10192cc6e255219e5a1f2ce626a7a
ichertc <feamahta@gmail.com>
2012-05-21 12:34:33 +0300

    first Automania commit. Login added

---
+++ Automania/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Automania::Application.config.secret_token = '8c2140aecf0239a69b2509e582626f19bc89012293956e26445c0987159bed9d35f09a7c112c0dd40c58bdce6668909f852727d7f874e29d757b616e199c2fd0'
